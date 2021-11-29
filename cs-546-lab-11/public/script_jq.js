1254a19cb32924f4613c7478f4dd6e4ed00b3e97
Christian Szablewski-Paz <32683410+cs-paz@users.noreply.github.com>
2021-11-29 13:43:29 -0500

    commit current lab 11

--- cs-546-lab-11/public/script_jq.js
+++ cs-546-lab-11/public/script_jq.js
@@ -1,50 +1,65 @@
-$( document ).ready(function() {
+$(document).ready(() => {
   const requestConfig = {
     method: 'GET',
     url: 'http://api.tvmaze.com/shows'
   };
-
-  $.ajax(requestConfig).then(function(responseMessage) {
+  $.ajax(requestConfig).then((responseMessage) => {
     var shows = $(responseMessage);
     $('#show').hide();
     $('#showList').show();
-    shows.each(function(index, show) { 
-      const elem = `<li><a href="${show.url}">${show.name}</a></li>`;
+    shows.each((index, show) => { 
+      const elem = `<li><a href="${show._links.self.href}">${show.name}</a></li>`;
       $('#showList').append(elem);
     });
+  }); 
+});
+
+$('#showList').on('click', 'li', (event) => {
+  event.preventDefault();
+  const showUrl = $(event.target).attr('href');
+  const requestConfig = {
+    method: 'GET',
+    url: showUrl
+  };
+  $.ajax(requestConfig).then((responseMessage) => {
+    console.log(responseMessage);
   });
+
+  $('#show').show();
+  $('#showList').hide();
 });
 
 $('#searchForm').submit((event) => {
-    event.preventDefault();
-    const search_term = $('#search_term').val().trim();
-    if (search_term && search_term.length != 0) {
-      $('#error').hide();
-      $('#showList').empty();
-      $('#showList').hide();
-      const requestConfig = {
-        method: 'GET',
-        url: `http://api.tvmaze.com/search/shows?q=${search_term}`
-      };
-      $.ajax(requestConfig).then(function(responseMessage) {
-        var shows = $(responseMessage);
-        if (shows.length === 0) {  
-          $('#error').show();
-          $('#error').text(`No results found for ${search_term}`);
-        } 
-        $('#show').hide();
-        $('#showList').show();
-        shows.each(function(index, show) { 
-          const elem = `<li><a href="${show.show.url}">${show.show.name}</a></li>`;
-          $('#showList').append(elem);
-        });
+  event.preventDefault();
+  const search_term = $('#search_term').val().trim();
+  if (search_term && search_term.length != 0) {
+    $('#reloadLink').show();
+    $('#error').hide();
+    $('#showList').empty();
+    $('#showList').hide();
+    const requestConfig = {
+      method: 'GET',
+      url: `http://api.tvmaze.com/search/shows?q=${search_term}`
+    };
+    $.ajax(requestConfig).then((responseMessage) => {
+      var shows = $(responseMessage);
+      if (shows.length === 0) {  
+        $('#error').show();
+        $('#error').text(`No results found for ${search_term}`);
+      } 
+      $('#show').hide();
+      $('#showList').show();
+      shows.each((index, show) => { 
+        const elem = `<li><a href="${show.show._links.self.href}">${show.show.name}</a></li>`;
+        $('#showList').append(elem);
       });
-      $('#search_term').focus();
-    } 
-    else {
-      $('#error').show();
-      $('#error').text(`Invalid Search Input!`);
-      $('#search_term').focus();
-      $('#search_term').val("");
-    }
-  });
+    });
+    $('#search_term').focus();
+  } 
+  else {
+    $('#error').show();
+    $('#error').text(`Invalid Search Input!`);
+    $('#search_term').focus();
+    $('#search_term').val("");
+  }
+});
